cd3094485a7894bd54055237e54d2baca5264575
Aleksandr Straton <houka1337@gmail.com>
2022-12-10 22:12:30 +0200

    designed the form

---
+++ notification/frm_notification.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace notification
+{
+    partial class frm_notification
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_close = new System.Windows.Forms.PictureBox();
+            this.txt_message = new System.Windows.Forms.Label();
+            this.picture_box = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_close)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture_box)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_close
+            // 
+            this.btn_close.Location = new System.Drawing.Point(338, 18);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(30, 30);
+            this.btn_close.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btn_close.TabIndex = 25;
+            this.btn_close.TabStop = false;
+            // 
+            // txt_message
+            // 
+            this.txt_message.AutoSize = true;
+            this.txt_message.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.txt_message.ForeColor = System.Drawing.Color.White;
+            this.txt_message.Location = new System.Drawing.Point(58, 21);
+            this.txt_message.Name = "txt_message";
+            this.txt_message.Size = new System.Drawing.Size(110, 21);
+            this.txt_message.TabIndex = 24;
+            this.txt_message.Text = "My message ...";
+            // 
+            // picture_box
+            // 
+            this.picture_box.Location = new System.Drawing.Point(12, 12);
+            this.picture_box.Name = "picture_box";
+            this.picture_box.Size = new System.Drawing.Size(40, 40);
+            this.picture_box.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picture_box.TabIndex = 23;
+            this.picture_box.TabStop = false;
+            // 
+            // frm_notification
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(36)))));
+            this.ClientSize = new System.Drawing.Size(380, 60);
+            this.Controls.Add(this.btn_close);
+            this.Controls.Add(this.txt_message);
+            this.Controls.Add(this.picture_box);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frm_notification";
+            this.Text = "frm_notification";
+            ((System.ComponentModel.ISupportInitialize)(this.btn_close)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture_box)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox btn_close;
+        internal System.Windows.Forms.Label txt_message;
+        private System.Windows.Forms.PictureBox picture_box;
+    }
+}
+
--- notification/frm_notification.cs
+++ notification/frm_notification.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace notification
 {
-    public partial class Form1 : Form
+    public partial class frm_notification : Form
     {
-        public Form1()
+        public frm_notification()
         {
             InitializeComponent();
         }
